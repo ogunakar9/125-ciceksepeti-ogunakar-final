a97d0d96aa59dcbaad74cd807db9f25e3ac856ec
ogunakar9 <ogunakar09@gmail.com>
2021-10-10 21:02:21 +0300

    add mock routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Users from "./pages/Users";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-      <h2>hi</h2>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/Users.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Users = () => {
+  return (
+    <>
+      <p>users</p>
+    </>
+  );
+};
+
+export default Users;
