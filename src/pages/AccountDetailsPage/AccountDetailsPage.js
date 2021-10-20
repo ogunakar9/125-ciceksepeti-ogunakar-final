ab665bc809d67b80507f22f5a1893651096e06dd
ogunakar9 <ogunakar09@gmail.com>
2021-10-20 22:53:20 +0300

    style account details except listbox cards

--- src/pages/AccountDetailsPage/AccountDetailsPage.js
+++ src/pages/AccountDetailsPage/AccountDetailsPage.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 import Header from "../../components/Header/Header";
 import { useDispatch, useSelector } from "react-redux";
 // import { useHistory } from "react-router-dom";
 import { acceptOffer, purchaseProduct, rejectOffer } from "../../store/actions";
+import profileIconBig from "../../assets/account-details/profile-icons/profile-icon-big.png";
 
 const AccountDetailsPage = () => {
   // const history = useHistory();
@@ -49,86 +50,83 @@ const AccountDetailsPage = () => {
     <>
       <Header />
       <div className="account-details_container">
-        <div style={{ width: "100%", height: "20%", backgroundColor: "#ddd" }}>
-          {email}
+        <div className="account-details_mail-wrapper">
+          <img src={profileIconBig} alt="profile-icon" />
+          <span>{email}</span>
         </div>
-        <div style={{ display: "flex", justifyContent: "flex-start" }}>
-          <div
-            onClick={handleReceived}
-            style={{
-              borderBottom: activeTab === "received" && "2px solid black",
-            }}
-          >
-            <p>Teklif Verdiklerim givenoffers</p>
+        <div className="account-details_list-container">
+          <div className="account-details_list_button-container">
+            <button
+              onClick={handleReceived}
+              className={
+                activeTab === "received" &&
+                "account-details_list_selected-button"
+              }
+            >
+              <p>Teklif Verdiklerim</p>
+            </button>
+            <button
+              onClick={handleGiven}
+              className={
+                activeTab === "given" && "account-details_list_selected-button"
+              }
+            >
+              <p>Teklif Aldıklarım</p>
+            </button>
           </div>
-          <div
-            onClick={handleGiven}
-            style={{
-              marginLeft: 50,
-              borderBottom: activeTab === "given" && "2px solid black",
-            }}
-          >
-            <p>Teklif aldiklarim received offers</p>
-          </div>
-        </div>
-        <div>
-          {activeTab === "received" &&
-            receivedOffers.map((item) => (
-              <div
-                key={item.id}
-                style={{ display: "flex", justifyContent: "space-between" }}
-              >
-                <div>
-                  <img
-                    src={item.product.imageUrl}
-                    style={{ height: 100, width: 100 }}
-                    alt={item.product.title}
-                  />
-                  <p>{item.product.price}</p>
-                </div>
-                <div>
-                  {!item.product.isSold && (
-                    <div>
-                      <button onClick={() => handleAcceptOffer(item.id)}>
-                        Onayla
-                      </button>
-                      <button onClick={() => handleRejectOffer(item.id)}>
-                        Reddet
-                      </button>
-                    </div>
-                  )}
-                </div>
-              </div>
-            ))}
-
-          {activeTab === "given" &&
-            givenOffers.map((item) => (
-              <div
-                key={item.id}
-                style={{ display: "flex", justifyContent: "space-between" }}
-              >
-                <div>
-                  <img
-                    src={item.product.imageUrl}
-                    style={{ height: 100, width: 100 }}
-                    alt={item.product.title}
-                  />
-                  <p>{item.product.price}</p>
-                </div>
-                <div>
+          <div className="account-details_list-box_wrapper">
+            {activeTab === "received" &&
+              receivedOffers.map((item) => (
+                <div key={item.id} className="account-details_list-box_card">
                   <div>
-                    {/*{!item.product.isSold && (*/}
-                    <button
-                      onClick={() => handlePurchaseOffered(item.product.id)}
-                    >
-                      Satin al
-                    </button>
-                    {/*)}*/}
-                    <span>{item.status}</span>
+                    <img
+                      src={item.product.imageUrl}
+                      style={{ height: 100, width: 100 }}
+                      alt={item.product.title}
+                    />
+                    <p>{item.product.price}</p>
+                  </div>
+                  <div>
+                    {!item.product.isSold && (
+                      <div>
+                        <button onClick={() => handleAcceptOffer(item.id)}>
+                          Onayla
+                        </button>
+                        <button onClick={() => handleRejectOffer(item.id)}>
+                          Reddet
+                        </button>
+                      </div>
+                    )}
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+
+            {activeTab === "given" &&
+              givenOffers.map((item) => (
+                <div key={item.id} className="account-details_list-box_card">
+                  <div>
+                    <img
+                      src={item.product.imageUrl}
+                      style={{ height: 100, width: 100 }}
+                      alt={item.product.title}
+                    />
+                    <p>{item.product.price}</p>
+                  </div>
+                  <div>
+                    <div>
+                      {/*{!item.product.isSold && (*/}
+                      <button
+                        onClick={() => handlePurchaseOffered(item.product.id)}
+                      >
+                        Satin al
+                      </button>
+                      {/*)}*/}
+                      <span>{item.status}</span>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </>
