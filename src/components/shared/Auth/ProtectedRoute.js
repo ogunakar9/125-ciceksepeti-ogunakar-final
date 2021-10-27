e335c7b7d58454f0183ba9e47e0dcadd306a45b7
ogunakar9 <ogunakar09@gmail.com>
2021-10-27 10:30:42 +0300

    remove all comments

--- src/components/shared/Auth/ProtectedRoute.js
+++ src/components/shared/Auth/ProtectedRoute.js
@@ -5,8 +5,6 @@ import { useSelector } from "react-redux";
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const { isSignedIn } = useSelector((state) => state.auth);
   return (
-    // Show the component only when the user is logged in
-    // Otherwise, redirect the user to /signin page
     <Route
       {...rest}
       render={(props) =>
